1cbc2279bf628dcf972c47f31543566640929b9c
shantanu400 <ashunayan400@gmail.com>
2024-07-15 14:51:54 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+async function getData(){
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const jsonData = await response.json();
+    setData(jsonData);
+    setLoading(false);
+  
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    setval("Error loading data");
+  }   
+}
+
+const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+ 
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const memoData = useMemo(() => data, [data]);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {memoData && memoData.map((item, index) => (
+        <div key={index}>
+          <h4>{item.title}</h4>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
   )
 }
